9d9888e3868dc9895f7c326d6a638419c17fd773
Daler <dsarifkulo021@gmail.com>
2023-07-19 21:02:05 +0500

    created some

---
+++ main.js
@@ -0,0 +1,73 @@
+import {
+    reloadCards,
+    createTransactionBox
+} from "./modules/ui";
+
+
+let cardBox = document.querySelector('.items-box')
+let table = document.querySelector('table')
+
+let fake = [{
+        name: "Visa",
+        currency: "RUB"
+    },
+    {
+        name: "Visa",
+        currency: "EUR"
+    }
+]
+let fakeT = [{
+        id: 465216,
+        type: "Visa",
+        date: '2023-01-15',
+        categories: 'Покупка продуктов',
+        sum: 1200
+    },
+    {
+        id: 465216,
+        type: "Visa",
+        date: '2023-07-14',
+        categories: 'Зачисление зарплаты',
+        sum: 35000
+    },
+    {
+        id: 465216,
+        type: "Visa",
+        date: '2023-07-12',
+        categories: 'Оплата счета за интернет',
+        sum: 800
+    },
+    {
+        id: 465216,
+        type: "Visa",
+        date: '2023-07-12',
+        categories: 'Оплата счета за интернет',
+        sum: 800
+    },
+    {
+        id: 465216,
+        type: "Visa",
+        date: '2023-07-12',
+        categories: 'Оплата счета за интернет',
+        sum: 800
+    },
+    {
+        id: 465216,
+        type: "Visa",
+        date: '2023-07-12',
+        categories: 'Оплата счета за интернет',
+        sum: 800
+    },
+    {
+        id: 465216,
+        type: "Visa",
+        date: '2023-07-12',
+        categories: 'Оплата счета за интернет',
+        sum: 800
+    },
+]
+
+
+reloadCards(fake, cardBox)
+
+createTransactionBox(fakeT, table, 4)
---
+++ modules/helpers.js
@@ -0,0 +1,46 @@
+export function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+export function handleClick(arg) {
+    if (arg.classList.contains("active")) {
+        arg.classList.remove("active")
+        arg.classList.add("close")
+    } else {
+        arg.classList.remove("close")
+        arg.classList.add("active")
+    }
+}
+
+export function populateTable(arr, place) {
+    place.innerHTML = ""
+    for (let item of arr) {
+        const row = place.insertRow(-1);
+        const id = row.insertCell(0);
+        const cart_type = row.insertCell(1);
+        const descriptionCell = row.insertCell(2);
+        const amountCell = row.insertCell(3);
+        const dateCell = row.insertCell(4);
+
+        id.innerHTML = item.id;
+        cart_type.innerHTML = item.type;
+        dateCell.innerHTML = `${(getDaysRemaining(item.date) * (-1))} дней назад`;
+        descriptionCell.innerHTML = item.categories;
+        amountCell.innerHTML = item.sum.toFixed(2);
+    }
+}
+
+export function getDaysRemaining(targetDateString) {
+    const targetDate = new Date(targetDateString);
+    const currentDate = new Date();
+
+    const timeDifference = targetDate.getTime() - currentDate.getTime();
+    const daysRemaining = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+
+    return daysRemaining;
+}
