2855eb07af331e6fea9616fb4df27cf3225914b7
SobirjonovAmir <amka7787@gmail.com>
2023-07-25 23:00:37 +0500

    .

---
+++ pages/transactions/index.js
@@ -0,0 +1,60 @@
+import {
+    createTransactionBox
+} from "/modules/ui.js"
+
+let userData = JSON.parse(localStorage.getItem("user"))
+document.querySelector("#user-email").textContent = userData.email
+
+let fakeT = [{
+    id: 465216,
+    type: "Visa",
+    date: '2023-01-15',
+    categories: 'Покупка продуктов',
+    sum: 1200
+},
+{
+    id: 465216,
+    type: "Visa",
+    date: '2023-07-14',
+    categories: 'Зачисление зарплаты',
+    sum: 35000
+},
+{
+    id: 465216,
+    type: "Visa",
+    date: '2023-07-12',
+    categories: 'Оплата счета за интернет',
+    sum: 800
+},
+{
+    id: 465216,
+    type: "Visa",
+    date: '2023-07-12',
+    categories: 'Оплата счета за интернет',
+    sum: 800
+},
+{
+    id: 465216,
+    type: "Visa",
+    date: '2023-07-12',
+    categories: 'Оплата счета за интернет',
+    sum: 800
+},
+{
+    id: 465216,
+    type: "Visa",
+    date: '2023-07-12',
+    categories: 'Оплата счета за интернет',
+    sum: 800
+},
+{
+    id: 465216,
+    type: "Visa",
+    date: '2023-07-12',
+    categories: 'Оплата счета за интернет',
+    sum: 800
+},
+]
+
+let tran_box = document.querySelector('table')
+createTransactionBox(fakeT, tran_box)
---
+++ pages/wallets/index.js
@@ -0,0 +1,17 @@
+import {
+	reloadCards,
+} from "/modules/ui";
+import {
+	getData
+} from "/modules/http"
+
+let cardBox = document.querySelector('.items-box')
+let userData = JSON.parse(localStorage.getItem("user"))
+document.querySelector("#user-email").textContent = userData.email
+
+getData("/cards?user_id=" + userData?.id)
+	.then(res => {
+		reloadCards(res.data, cardBox)
+	})
+
+
